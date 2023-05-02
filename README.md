3d7323ac46804384a9db73ff74807fade762e191
QuantStats <wei@DESKTOP-6T98IQR>
2023-05-03 00:43:45 +0200

    Working first version.

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # SudokuOCR
 
 UI is built with Flask - a user uploads an image file, display detected Sudoku puzzle result and its solution.
+
 OCR Model is built with TensorFlow - a CNN model trained on the MNIST, printed digit and Sudoku clipping dataset.
 
 ## References
 https://aakashjhawar.medium.com/sudoku-solver-using-opencv-and-dl-part-1-490f08701179
+
 https://www.kaggle.com/datasets/dhruvmomoman/printed-digits
+
 https://www.kaggle.com/datasets/rohit369/sudoku-digit-classification
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import uuid
+import cv2
+import base64
+import numpy as np
+import tensorflow as tf
+import pandas as pd
+from pathlib import Path
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = Path('uploads')
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # 5 MB max upload size
+
+app.config['UPLOAD_FOLDER'].mkdir(parents=True, exist_ok=True)
+
+# Load your saved TensorFlow model
+model = tf.keras.models.load_model('models/digit_recognition_model5.h5')
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+def convert_image_to_base64(img):
+    _, buffer = cv2.imencode('.png', img)
+    return base64.b64encode(buffer).decode("utf-8")
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_image():
+    image_base64 = None
+    output_table = None
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return "No file part", 400
+        file = request.files['file']
+        if file.filename == '':
+            return "No selected file", 400
+        if file and allowed_file(file.filename):
+            img_data = file.read()
+            output = np.random.rand(3, 3)
+            output_table = pd.DataFrame(output).to_html(header=False, index=False)
+
+            # Decode the image again for displaying
+            np_arr = np.frombuffer(img_data, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            image_base64 = convert_image_to_base64(img)
+
+    return render_template('index.html', image_base64=image_base64, output_table=output_table)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
